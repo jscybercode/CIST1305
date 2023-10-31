ba3468fd6ac0adf4a7346b3dfbd8029f13d03651
jscybercode <139887590+jscybercode@users.noreply.github.com>
2023-10-31 19:37:09 -0400

    all new folders
    This should work

--- oct31Cist1510tuesClass/javascript/script.js
+++ oct31Cist1510tuesClass/javascript/script.js
@@ -1,21 +1,6 @@
-// function hamburger() {
-//     var menu = document.getElementById("menu-links");
-//     var logo = document.getElementById("ffc-logo");
-//     if (menu.style.display === "block" && logo.style.display === "none") {
-//         menu.style.display = "none";
-//         logo.style.display = "block";
-//     } else {
-//         menu.style.display = "block";
-//         logo.style.display = "none";
-//     }
-// }
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
