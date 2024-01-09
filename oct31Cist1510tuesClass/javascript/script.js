1831646351d10f2e9c3637bf3ec7aca9aa31ac7c
jscybercode <139887590+jscybercode@users.noreply.github.com>
2024-01-08 22:45:18 -0500

    Delete oct31Cist1510tuesClass directory

--- oct31Cist1510tuesClass/javascript/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
-
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
